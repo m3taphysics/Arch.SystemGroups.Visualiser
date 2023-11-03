499b57fc735c770bd6b0b0ac32cc313b6e428661
Ashley Canning <ashley.canning@gmail.com>
2023-11-03 15:57:32 +0000

    Initial implementation of the group and system visualisation (#1)

--- Assets/Scripts/Systems/InitSystemGroupSystem.cs
+++ Assets/Scripts/Systems/InitSystemGroupSystem.cs
@@ -12,4 +12,23 @@ namespace Systems
         {
         }
     }
+    
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [UpdateAfter(typeof(InitSystemGroupSystem))]
+    public partial class AfterInitSystemGroupSystem : BaseSystem<World, float>
+    {
+        public AfterInitSystemGroupSystem(World world) : base(world)
+        {
+        }
+    }
+    
+    
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    [UpdateAfter(typeof(AfterInitSystemGroupSystem))]
+    public partial class AfterAfterInitSystemGroupSystem : BaseSystem<World, float>
+    {
+        public AfterAfterInitSystemGroupSystem(World world) : base(world)
+        {
+        }
+    }
 }
